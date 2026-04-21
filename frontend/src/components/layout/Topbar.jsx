4a71cecba7a9694af29b915762a9a2ce9138aace
jakes007 <jason.isaacs@hotmail.co.za>
2026-04-21 15:52:32 +0200

    changed scores colours and also added a motto in the topbar

--- frontend/src/components/layout/Topbar.jsx
+++ frontend/src/components/layout/Topbar.jsx
@@ -4,9 +4,14 @@ export default function Topbar({ onMenuClick }) {
   return (
     <header className="topbar">
       <div className="topbar-row">
-        <div className="topbar-brand">
-          <img src={logo} alt="ODA Logo" className="topbar-logo" />
-        </div>
+      <div className="topbar-brand">
+  <img src={logo} alt="ODA Logo" className="topbar-logo" />
+
+  <div className="topbar-motto">
+    <span className="topbar-divider" />
+    <span className="topbar-motto-text">"The Home of Champions"</span>
+  </div>
+</div>
 
         <button
           className="mobile-menu-btn"
